aa92c72dce9508c940464813b2342ce5d84a74aa
Jacob Martin <ocornv2.0@gmail.com>
2025-12-10 10:35:00 -0500

    CORS take 2

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app = FastAPI(
 app.add_middleware(
 	CORSMiddleware,
 	allow_origins=["*"],  # Allows all origins
-	allow_credentials=True,
+	allow_credentials=False,
 	allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
 	allow_headers=["*"],  # Allows all headers (Authorization, etc.)
 )
